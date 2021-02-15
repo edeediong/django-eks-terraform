b5c9def53490028a51ab967b9db5a9fc016f2196
Edidiong Etuk <edidiongmichael08@gmail.com>
2021-02-15 12:04:59 +0100

    Added Django & Terraform files

---
+++ infrastructure/provider.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.28"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.0.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+
+  required_version = "~> 0.14"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}

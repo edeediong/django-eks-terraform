a10f0659202774db888275427e92a0e02c3dc592
Edidiong Etuk <edidiongmichael08@gmail.com>
2022-06-03 17:55:26 +0100

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "3.1.3"
     }
 
     local = {
